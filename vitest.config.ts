5e6108b41f4c275ea1c00ac761d1dff1d3cd457d
sunub <bsc5672@gmail.com>
2026-05-16 15:56:02 +0900

    feat(test) : 디자인 시스템 내의 컴포넌트 기능 테스트를 위한 vitest 설정 추가

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,19 @@
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [],
-	resolve: {
-		tsconfigPaths: true,
-	},
+	plugins: [vanillaExtractPlugin()],
 	test: {
 		name: "unit",
-		include: ["tests/unit/**/*.{test,spec}.{ts,tsx}"],
+		include: [
+			"tests/unit/**/*.{test,spec}.{ts,tsx}",
+			"packages/*/src/**/*.{test,spec}.{ts,tsx}",
+		],
 		exclude: ["node_modules/**", "dist/**"],
 		globals: true,
-		environment: "node",
+		environment: "jsdom",
 		coverage: {
+			provider: "v8",
 			reporter: ["text", "json-summary", "json", "html", "lcovonly"],
 			thresholds: {
 				lines: 60,
