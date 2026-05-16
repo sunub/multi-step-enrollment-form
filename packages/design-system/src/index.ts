3b2511078c70c8a4e45465a57cb35aa1fc48ceb9
sunub <bsc5672@gmail.com>
2026-05-16 15:51:42 +0900

    feat(design-system/components): 다형성을 지원하는 핵심 UI 컴포넌트 구현
    - Box, Flex, Grid를 통해 복잡한 레이아웃을 선언적으로 구성할 수 있는
    기반 마련
    - @radix-ui/react-slot 및 as Prop을 도입하여 컴포넌트의 시맨틱 태그를
    자유롭게 변경하면서 스타일 일관성 유지할 수 있게끔 구현.

---
+++ packages/design-system/src/index.ts
@@ -0,0 +1,23 @@
+export * from "./components";
+
+export {
+  Box,
+  type BoxProps,
+} from "./primitives/Box";
+export {
+  Flex,
+  type FlexProps,
+} from "./primitives/Flex";
+export {
+  Grid,
+  type GridProps,
+} from "./primitives/Grid";
+export {
+  splitProps,
+  sprinklePropNames,
+} from "./styles/sprinkels.utils";
+export {
+  type BaseStyle,
+  baseStyles,
+} from "./styles/sprinkles.css";
+export { vars } from "./tokens/theme.css";
