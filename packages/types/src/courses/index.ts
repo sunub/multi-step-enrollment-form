96f35d6bf7325acc488a9e8fd6172a5605e935dc
sunub <bsc5672@gmail.com>
2026-05-18 02:28:57 +0900

    refactor(packages/type) : constants 와 중복되는 변수 선언 제거

--- packages/types/src/courses/index.ts
+++ packages/types/src/courses/index.ts
@@ -1,15 +1,17 @@
 import { z } from "zod";
 
-export const COURSE_CATEGORIES = [
+const COURSE_CATEGORIES = [
 	"development",
 	"design",
 	"marketing",
 	"business",
 ] as const;
 
-export const CourseTypeSchema = z.enum(COURSE_CATEGORIES);
+export type CourseCategoryType = (typeof COURSE_CATEGORIES)[number];
 
-export type CourseType = z.infer<typeof CourseTypeSchema>;
+export const CourseCategorySchema = z.enum(COURSE_CATEGORIES);
+
+export const CourseTypeSchema = z.enum(COURSE_CATEGORIES);
 
 export const CourseSchema = z.object({
 	id: z.string(),
@@ -23,3 +25,5 @@ export const CourseSchema = z.object({
 	endDate: z.string(), // ISO 8601
 	instructor: z.string(),
 });
+
+export type CourseType = z.infer<typeof CourseSchema>;
